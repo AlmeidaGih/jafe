a29310aada734b06dfc8741619539a148f0831b4
AlmeidaGih <giovanna.almeida.sousa1@gmail.com>
2023-10-10 21:10:06 -0300

    ínicio da home do usuário

--- app/src/main/java/com/example/jafeproject/UserMenuView.kt
+++ app/src/main/java/com/example/jafeproject/UserMenuView.kt
@@ -3,8 +3,7 @@ package com.example.jafeproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class UserMenuView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_menu)
